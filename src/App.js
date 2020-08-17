57a77d5193304b86b1af12650854624e5cefc9b1
Pedro <pedrojr_dantas@hotmail.com>
2020-08-17 14:25:38 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import "./App.css"
 
-function App() {
+
+class App extends Component{
+  state={
+      reset:[],
+      numbers:[]
+    }
+  
+
+  generateNumbers = () =>{
+      const lotery6 = [];
+      for (let i=0; i < 6; i++) {
+      let num = Math.floor(Math.random() * 49);
+      if (lotery6.includes(num)) {
+        num = Math.floor(Math.random() * 49);
+      }
+      lotery6.push(num);
+      
+   }
+   console.log(lotery6);
+   this.setState({numbers:lotery6});
+  };
+
+  newNumbers = () => {
+    this.setState({numbers:this.state.reset})
+  }
+  render () {
   return (
     <div className="App">
-      <h1>My React App</h1>
+      <div>  {this.state.numbers.map((item) => {
+        return (
+          <div id="items">{item}</div>
+        )
+      })}
+      </div>
+      <div>
+        <button onClick={this.newNumbers}>reset</button>
+        <button onClick={this.generateNumbers}>Try-out</button>
+      </div>
+   
     </div>
   );
+}
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 import React from "react"
 import App from "./App.js"
-import reactDOM from "react-dom"
+import ReactDOM from "react-dom"
 
-reactDOM.render(<App/>,document.getElementById("root"))
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
